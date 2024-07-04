bb840042149544541b768a32d328696ee31869c6
Olexandr597 <Olexandr597@gmail.com>
2024-07-04 16:54:44 +0300

    remove last

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,26 @@
-export const removeLastContact = async () => {};
+import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-removeLastContact();
+export const removeLastContact = async () => {
+  try {
+    const fileContent = await fs.readFile(PATH_DB, 'utf8');
+    const fileContentRemoveLast = JSON.parse(fileContent);
+
+    if (!Array.isArray(fileContentRemoveLast)) {
+      throw new Error('Дані бази даних не є масивом');
+    }
+
+    if (fileContentRemoveLast.length > 0) {
+      fileContentRemoveLast.pop();
+      await fs.writeFile(
+        PATH_DB,
+        JSON.stringify(fileContentRemoveLast, null, 2),
+        'utf8',
+      );
+    }
+  } catch (err) {
+    console.error('Помилка видалення контакту з файлу:', err);
+  }
+};
+
+await removeLastContact();
